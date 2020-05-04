5fb466f5f4b4df0b7b7efd33d4a6fcbe1051e365
Felipe Rocha <felipecortesrocha@gmail.com>
2020-05-04 12:24:28 -0300

    Finalizaçao front

--- frontent/src/pages/Profile/index.js
+++ frontent/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { FiPower, FiTrash2 } from 'react-icons/fi'
 
 import api from '../../services/api'
@@ -10,6 +10,8 @@ import logoImg from '../../assets/logo.svg'
 
 export default function Profile() {
     const [incidents, setIncidents] = useState([])
+
+    const history = useHistory()
 
     const ongId = localStorage.getItem('ongId')
     const ongName = localStorage.getItem('ongName')
@@ -31,9 +33,17 @@ export default function Profile() {
                     Authorization: ongId
                 }
             })
+
+            setIncidents(incidents.filter(incident => incident.id !== id))
         } catch (err) {
             alert('Erro ao deletar caso, tente novamente.')
         }
+    }
+
+    function handleLogout() {
+        localStorage.clear()
+
+        history.push('/')
     }
 
     return (
@@ -44,7 +54,7 @@ export default function Profile() {
 
                 <Link className='button' to='/incidents/new'>Cadastrar novo caso</Link>
 
-                <button type='button'>
+                <button onClick={handleLogout} type='button'>
                     <FiPower size={18} color='#e02041' />
                 </button>
             </header>
